ebe34ca4dcf0c7a39f26cf8b8c6342b30a2f0ef5
Mad Xian <julianmullensb@gmail.com>
2025-10-21 11:20:30 +0000

    File changes

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -1,17 +1,21 @@
 // IMPORTANT: This file is used on the SERVER-SIDE only.
 // It uses the Firebase Admin SDK to connect to Firestore.
 
-import { initializeApp, getApps } from 'firebase-admin/app';
+import { initializeApp, getApps, App } from 'firebase-admin/app';
 import { getFirestore } from 'firebase-admin/firestore';
+
+let app: App;
 
 // In the App Hosting environment, the SDK will automatically discover the
 // service account credentials. When initializeApp() is called with no arguments,
 // it uses GOOGLE_APPLICATION_CREDENTIALS environment variable.
 // App Hosting automatically sets this variable.
 if (!getApps().length) {
-  initializeApp();
+  app = initializeApp();
+} else {
+  app = getApps()[0];
 }
 
-const adminDb = getFirestore();
+const adminDb = getFirestore(app);
 
 export { adminDb };
